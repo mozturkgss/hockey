87408870f76f4789abb02354ba930a2b379fab0d
mozturkgss <mozturkgss@gmail.com>
2025-03-28 13:40:26 +0300

    update to godot 4.3

--- Menu.gd
+++ Menu.gd
@@ -1,35 +1,8 @@
-extends CanvasLayer
+extends Node
 
-signal start_game
-
-func _ready():
-	$TimerLabel.hide()
-	$Score1Label.hide()
-	$Score2Label.hide()
+func _process(delta: float) -> void:
+	pass
 	
-func show_message(text):
-	$MessageLabel.text = text
-	$MessageLabel.show()
-	$MessageTimer.start()
-	
-func show_game_over():
-	show_message("Game Over")
-	yield($MessageTimer, "timeout")
-	$StartButton.show()
-	$MessageLabel.text = "Hokey"
-	$MessageLabel.show()
-	
-func update_score(timer, score1, score2):
-	$TimerLabel.text = str(timer)
-	$Score1Label.text = str(score1)
-	$Score2Label.text = str(score2)
-
-func _on_StartButton_pressed():
-	$StartButton.hide()
-	$TimerLabel.show()
-	$Score1Label.show()
-	$Score2Label.show()
-	emit_signal("start_game")
-
-func _on_MessageTimer_timeout():
-	$MessageLabel.hide()
+func _on_pressed():
+	$ClickSound.play()	
+	get_tree().change_scene_to_file("res://game.tscn")
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Hockey game for Android and IOS
 
-Written in Godot 3
+Written in Godot 4.3
 
 Android Demo: https://play.google.com/store/apps/details?id=com.hokeygame
